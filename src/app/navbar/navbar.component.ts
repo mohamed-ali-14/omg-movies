ba22a5b1e245c28f152f03530da548e8bc10dda7
unknown <mohamed458714@gmail.com>
2022-11-10 06:01:32 +0200

    edit2

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+
 import { AuthService } from '../auth.service';
 import { SearchService } from '../search.service';
 
